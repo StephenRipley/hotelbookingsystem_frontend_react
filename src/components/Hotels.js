bafff4426d1795a87c227f57e505d65065b51d23
Colin Robertson <colin.robertson@fdmgroup.com>
2020-07-03 14:48:27 +0100

    added pagination to the list of hotels

--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactPaginate from 'react-paginate'
 import axios from 'axios';
 import HotelList from './HotelList';
 import Error from './Error';
@@ -8,41 +9,68 @@ class Hotels extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            offset: 0,
             hotels: [],
+            perPage: 5,
+            currentPage: 0,
             error: false,
         };
+        this.handlePageClick = this.handlePageClick.bind(this);
     }
 
-    componentDidMount() {
-        const url = "http://localhost:8088/hotelbookingsystem/admin/AllHotels";
+    recievedData() {
+      const url = "http://localhost:8088/hotelbookingsystem/admin/AllHotels";
     
         axios.get(url)
-        .then((response) => {
+        .then(response => {
+
+          const hotels = response.data;
+          const slice = hotels.slice(this.state.offset, this.state.offset + this.state.perPage)
+          const postData = slice.map((hotel) => <React.Fragment>
+            <HotelList key={hotel.hotelId} hotel={hotel} /></React.Fragment>)
+
           this.setState({
-            hotels: response.data
-          })
-        })
-        .catch((error) => {
-          this.setState({
-            error: true
+            pageCount: Math.ceil(hotels.length / this.state.perPage),
+
+            postData
           })
         });
+        
       }
+      handlePageClick = (e) =>{
+        const selectedPage = e.selected;
+        const offset = selectedPage * this.state.perPage;
 
-    renderItems() {
-        if(!this.state.error) {
-          return this.state.hotels.map((hotel) => (
-            <HotelList key={hotel.hotelId} hotel={hotel} />
-          ));
-        } else {
-            return <Error />
-          }
-      }
+        this.setState({
+          currentPage: selectedPage,
+          offset: offset
+        }, () => {
+          this.recievedData()
+        });
+      };
+    
+
+    componentDidMount() {
+      this.recievedData()
+    }
+        
 
     render() {
         return (
           <div className="row">
-            {this.renderItems()}
+           {this.state.postData}
+           <ReactPaginate
+              previousLabel={"prev"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={this.state.pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={this.handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"}/>
           </div>
         );
       }
