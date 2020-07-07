dbad957f5b8bfd186c40cb2437c0e7fa65fe95ee
Colin Robertson <colin.robertson@fdmgroup.com>
2020-07-07 14:24:16 +0100

    added navbar and some routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import Hotels from './Hotels';
 import Login from './Login';
+import NavBar from './NavBar';
 
 class App extends Component {
   constructor(props) {
@@ -17,12 +22,15 @@ class App extends Component {
         <div className="nav-wrapper indigo lighten-4">
               <h1>Hotel Booking System</h1>
         </div>
+        <Router>
+        <NavBar />
         <div className="row">
           <div className="col s8">
-            <Login/>
-            <Hotels/>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/hotels" component={Hotels} />
           </div>
         </div>
+        </Router>
       </div>
     );
   }
